36c435fd20e29f3b35bb3eaa1b51fd3d80689a46
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2022-07-12 17:43:11 +0800

    Updated API Services

--- fake_store_api/lib/services/api_service.dart
+++ fake_store_api/lib/services/api_service.dart
@@ -1,3 +1,68 @@
+import 'package:fake_store_api/models/product.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import '../models/cart.dart';
+
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
+
+  Future<List<String>> getAllCategories() async {
+    return http.get(Uri.parse('$baseUrl/products/categories')).then((data) {
+      final categories = <String>[];
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        for (var item in jsonData) {
+          categories.add(item);
+        }
+      }
+      return categories;
+    }).catchError((err) => print(err));
+  }
+
+  Future<List<Product>> getAllProducts() async {
+    return http.get(Uri.parse('$baseUrl/products')).then((data) {
+      final products = <Product>[];
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        for (var product in jsonData) {
+          products.add(Product.fromJson(product));
+        }
+      }
+      return products;
+    }).catchError((err) => print(err));
+  }
+
+  Future<Product> getProduct(int id) async {
+    return http.get(Uri.parse('$baseUrl/products/$id')).then((data) {
+      Product product = Product();
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        product = Product.fromJson(jsonData);
+      }
+      return product;
+    }).catchError((err) => print(err));
+  }
+
+  // Future<Cart> updateCart(int userId, int id) async {
+  //   return http.put(Uri.parse('$baseUrl/carts/$id'), 
+  //     body: json.encode(id.toJson())).then((data) {
+  //     Cart cart = Cart();
+  //     if (data.statusCode == 204) {
+  //       final jsonData = json.encode(data.body);
+  //     }
+  //     return cart;
+  //   }).catchError((err) => print(err));
+  // }
+
+  Future<Cart?> getCart(int userId) async {
+    return http.get(Uri.parse('$baseUrl/carts/$userId')).then((data) {
+      Cart cart = Cart();
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        cart = Cart.fromJson(jsonData);
+      }
+      return cart;
+    }).catchError((err) => print(err));
+  }
 }
